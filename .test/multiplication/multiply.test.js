365d28a4107812de2d25d4d34928232e30322c8d
Jonas Geertsen Lund <89989380+JonasGLund99@users.noreply.github.com>
2022-05-19 19:10:21 +0200

    testing commit

---
+++ .test/multiplication/multiply.test.js
@@ -0,0 +1,12 @@
+const { multiplyTwo } = require('../../multiplication/multiply.js');
+
+const { multiplyThree } = require('../../multiplication/multiply.js');
+
+test('Multiplying two numbers <multiplyTwo>', async () => {
+    const result = await multiplyTwo(5,10);
+    expect(result).toStrictEqual(50);
+})
+test('Multiplying three numbers <multiplyThree>', async () => {
+    const result = await multiplyThree(2,3,4);
+    expect(result).toStrictEqual(24);
+})
---
+++ .test/sum.test.js
@@ -0,0 +1,16 @@
+const { sumTwo } = require('../sum.js');
+
+const { sumThree } = require('../sum.js');
+
+test('Sum two numbers <sumTwo>', async () => {
+    const result = await sumTwo(22,10);
+    expect(result).toStrictEqual(32);
+})
+test('Sum three numbers <sumThree>', async () => {
+    const result = await sumThree(10,20,30);
+    expect(result).toStrictEqual(60);
+})
+test('Sum three numbers (negative) <sumThree>', async () => {
+    const result = await sumThree(-10,-10,-10);
+    expect(result).toStrictEqual(-30);
+})
