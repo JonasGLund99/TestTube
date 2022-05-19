af10788a19499410ae47f2bd69ca7a2a3ee12e63
Jonas Geertsen Lund <89989380+JonasGLund99@users.noreply.github.com>
2022-05-19 19:37:07 +0200

    testing commit

--- .test/multiplication/multiply.test.js
+++ .test/multiplication/multiply.test.js
@@ -6,6 +6,10 @@ test('Multiplying two numbers <multiplyTwo>', async () => {
     const result = await multiplyTwo(5,10);
     expect(result).toStrictEqual(50);
 })
+test('Another one for multiply <multiplyTwo>', async () => {
+    const result = await multiplyTwo(9,7);
+    expect(result).toStrictEqual(63);
+})
 test('Multiplying three numbers <multiplyThree>', async () => {
     const result = await multiplyThree(2,3,4);
     expect(result).toStrictEqual(24);
