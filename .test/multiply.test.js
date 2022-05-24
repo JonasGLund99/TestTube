5c6c99034e0cb333e76eb88e48b3b35d7cc943e6
Thorbjørn Møller <66469783+thor1878@users.noreply.github.com>
2022-05-24 10:14:03 +0200

    Commit by TestTube

--- .test/multiply.test.js
+++ .test/multiply.test.js
@@ -4,3 +4,7 @@ test('Multiply two numbers <multiply>', async () => {
     const result = await multiply(1,2);
     expect(result).toBe(2);
 })
+test('Multiply another two numbers <multiply>', async () => {
+    const result = await multiply(2,3);
+    expect(result).toBe(6);
+})
