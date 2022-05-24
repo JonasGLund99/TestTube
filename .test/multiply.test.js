68086c7db6f9cba583d7c337ef972192db351de8
Thorbjørn Møller <66469783+thor1878@users.noreply.github.com>
2022-05-24 10:29:23 +0200

    Commit by TestTube

--- .test/multiply.test.js
+++ .test/multiply.test.js
@@ -4,3 +4,7 @@ test('Multiply two numbers <multiply>', async () => {
     const result = await multiply(1,2);
     expect(result).toBe(2);
 })
+test('Multiply another two numbers <multiply>', async () => {
+    const result = await multiply(1,1);
+    expect(result).toBe(1);
+})
--- .test/sum.test.js
+++ .test/sum.test.js
@@ -4,3 +4,7 @@ test('Sum two positive numbers <sum>', async () => {
     const result = await sum(1,2);
     expect(result).toBe(3);
 })
+test('Sum two negative numbers <sum>', async () => {
+    const result = await sum(-1,-2);
+    expect(result).toBe(-3);
+})
