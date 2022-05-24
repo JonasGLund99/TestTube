007b3a35a7f99f10e847887ff8db2e0a83ac3196
Thorbjørn Møller <66469783+thor1878@users.noreply.github.com>
2022-05-24 11:12:31 +0200

    Commit by TestTube

--- .test/sum.test.js
+++ .test/sum.test.js
@@ -1,11 +1,16 @@
 const { sum } = require('../sum.js');
 
+const { sumThree } = require('../sum.js');
+
 test('Sum two positive numbers <sum>', async () => {
     const result = await sum(1,2);
     expect(result).toBe(3);
 })
-
 test('Sum two negative numbers <sum>', async () => {
     const result = await sum(-1,-2);
     expect(result).toBe(-3);
+})
+test('Sum three numbers <sumThree>', async () => {
+    const result = await sumThree(1,2,3);
+    expect(result).toBe(6);
 })
