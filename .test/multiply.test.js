5589406934bffeedc32cb57e820cbfc58ee117ca
thor1878 <no-email>
2022-05-24 09:24:03 +0000

    merge conflict resolved

--- .test/multiply.test.js
+++ .test/multiply.test.js
@@ -4,7 +4,6 @@ test('Multiply two numbers <multiply>', async () => {
     const result = await multiply(1,2);
     expect(result).toBe(2);
 })
-
 test('Multiply another two <multiply>', async () => {
     const result = await multiply(1,1);
     expect(result).toBe(1);
