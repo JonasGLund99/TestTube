a9a4f62cbc437cae7ba2b15ebeac7263e2effd0d
Thorbjørn Møller <66469783+thor1878@users.noreply.github.com>
2022-05-24 22:00:01 +0200

    Commit by TestTube

--- .test/sum.test.js
+++ .test/sum.test.js
@@ -4,3 +4,7 @@ test('sum two numbers <sum>', async () => {
     const result = await sum(1,1);
     expect(result).toBe(2);
 })
+test('Sum another two numbers <sum>', async () => {
+    const result = await sum(1,2);
+    expect(result).toBe(3);
+})
