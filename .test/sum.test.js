d221529478d4999f95baa912d19e18db5a7903e7
Xaniah <54134770+Xaniah@users.noreply.github.com>
2022-05-24 10:56:43 +0200

    Update sum.test.js

--- .test/sum.test.js
+++ .test/sum.test.js
@@ -4,10 +4,8 @@ test('Sum two positive numbers <sum>', async () => {
     const result = await sum(1,2);
     expect(result).toBe(3);
 })
-<<<<<<< HEAD
-=======
+
 test('Sum two negative numbers <sum>', async () => {
     const result = await sum(-1,-2);
     expect(result).toBe(-3);
 })
->>>>>>> fbe4898 (Commit by TestTube)
