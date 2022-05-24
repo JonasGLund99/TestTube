9586b3543b0e80f9ab098f9a16ec1e551eb59178
thor1878 <no-email>
2022-05-24 09:28:50 +0000

    merge conflict resolved

--- .test/divide.test.js
+++ .test/divide.test.js
@@ -4,3 +4,7 @@ test('Divide two numbers <divide>', async () => {
     const result = await divide(1,1);
     expect(result).toBe(1);
 })
+test('Divideded <divide>', async () => {
+    const result = await divide(2,2);
+    expect(result).toBe(1);
+})
--- .test/sum.test.js
+++ .test/sum.test.js
@@ -4,7 +4,6 @@ test('Sum two positive numbers <sum>', async () => {
     const result = await sum(1,2);
     expect(result).toBe(3);
 })
-
 test('Sum two negative numbers <sum>', async () => {
     const result = await sum(-1,-2);
     expect(result).toBe(-3);
