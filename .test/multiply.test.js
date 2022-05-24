18a900aa04feaf18e1062b25a30b9c8b160fdd1b
thor1878 <no-email>
2022-05-24 14:06:48 +0000

    merge conflict resolved

--- .test/multiply.test.js
+++ .test/multiply.test.js
@@ -4,3 +4,7 @@ test('Multiply two numbers <multiply>', async () => {
     const result = await multiply(4,5);
     expect(result).toBe(20);
 })
+test('Multiply sum more numbers <multiply>', async () => {
+    const result = await multiply(1,1);
+    expect(result).toBe(1);
+})
--- .test/sum.test.js
+++ .test/sum.test.js
@@ -4,3 +4,7 @@ test('Sum two numbers <sum>', async () => {
     const result = await sum(1,2);
     expect(result).toBe(3);
 })
+test('Sum two more nice numbers <sum>', async () => {
+    const result = await sum(1,1);
+    expect(result).toBe(2);
+})
