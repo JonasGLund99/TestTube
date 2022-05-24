0c1d28e51b23948354b75aaa25e00a1817695cac
thor1878 <no-email>
2022-05-24 08:41:01 +0000

    merge conflict resolved

--- .test/sum.test.js
+++ .test/sum.test.js
@@ -5,9 +5,15 @@ test('Sum two positive numbers <sum>', async () => {
     expect(result).toBe(3);
 })
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
+=======
+>>>>>>> 89c7c00 (Commit by TestTube)
 test('Sum two negative numbers <sum>', async () => {
     const result = await sum(-1,-2);
     expect(result).toBe(-3);
 })
+<<<<<<< HEAD
 >>>>>>> fbe4898 (Commit by TestTube)
+=======
+>>>>>>> 89c7c00 (Commit by TestTube)
