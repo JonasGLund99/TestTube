aa39e264476e36f76ee999411eb4b78f7abb2b01
Jonas Geertsen Lund <89989380+JonasGLund99@users.noreply.github.com>
2022-06-20 08:49:53 +0200

    Commit by TestTube

--- .test/sum.test.js
+++ .test/sum.test.js
@@ -2,7 +2,13 @@ const { sumTwo } = require('../sum.js');
 
 const { sumThree } = require('../sum.js');
 
+const { sumFour } = require('../sum.js');
+
 test('Sum dis two numbers <sumTwo>', async () => {
     const result = await sumTwo(20,49);
     expect(result).toEqual(69);
+})
+test('Sum four (this will also fail) <sumFour>', async () => {
+    const result = await sumFour(2,3,4,5);
+    expect(result).toBe(1);
 })
