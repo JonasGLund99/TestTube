232caafcbdb724823ed765c0c89940ba6924e7c0
Johannes Karstoft Pedersen <93575274+Jojokp2000@users.noreply.github.com>
2022-06-21 14:18:43 +0200

    Commit by TestTube

--- .test/square.test.js
+++ .test/square.test.js
@@ -1,2 +1,6 @@
 const { square } = require('../square.js');
 
+test('Manipulate object with numbers <square>', async () => {
+    const result = await square({);
+    expect(result).toEqual([ { functionString: ´function generateString1(input1) { const );
+})
